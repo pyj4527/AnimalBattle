2bea04080f1aefbf47d42f770b70700704004588
chaeyeoni0418 <chaeyeoni0418@gmail.com>
2026-06-05 09:38:53 +0900

    Launch GUI from animal main

--- src/animal/Main.java
+++ src/animal/Main.java
@@ -1,36 +1,9 @@
 package animal;
 
+import smarthome.start;
+
 public class Main {
 	public static void main(String[] args) {
-		GameManager gameManager = new GameManager(GameManager.createDefaultAnimals());
-		Animal winner = null;
-		int turn = 1;
-
-		while (winner == null) {
-			System.out.println("=== " + turn + "턴 ===");
-			GameManager.DiceTurnResult diceTurnResult = gameManager.rollDiceTurn();
-			System.out.println(diceTurnResult.toMessage());
-			gameManager.prepareItemPhaseAfterDice();
-
-			winner = gameManager.findWinner();
-			while (winner == null && gameManager.hasCurrentItemUser()) {
-				Animal itemUser = gameManager.getCurrentItemUser();
-				GameManager.ItemResult itemResult = gameManager.useRandomItem(itemUser);
-				System.out.println(itemResult.toMessage());
-				winner = gameManager.findWinner();
-				gameManager.moveToNextItemUser();
-			}
-			gameManager.finishItemPhase();
-			printStatus(gameManager);
-			turn++;
-		}
-
-		System.out.println("승리 동물: " + winner.getName());
-	}
-
-	private static void printStatus(GameManager gameManager) {
-		for (Animal animal : gameManager.getAnimals()) {
-			System.out.println(animal.getName() + ": " + animal.getDistance() + "/" + Animal.GOAL_DISTANCE);
-		}
+		new start().setVisible(true);
 	}
 }
